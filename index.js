6c2e2bf5a7b8b2ec655387f20242d3bfaf646228
Zohorl <zohorul109561@gmail.com>
2022-05-26 15:06:18 +0600

    added user

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ async function run() {
         await client.connect();
         const toolCollection = client.db('best_tools').collection('tools');
         const reviewCollection = client.db('best_tools').collection('reviews');
+        const userCollection = client.db('best_tools').collection('users');
+        // const bookingCollection = client.db('best_tools').collection('booking');
 
         app.get('/tool', async (req, res) => {
             const query = {};
@@ -37,6 +39,57 @@ async function run() {
             const reviews = await reviewCollection.find().toArray();
             res.send(reviews);
         });
+
+        // PUT
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result });
+        });
+        // 
+        // app.get('/tool/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const item = await toolCollection.findOne(query);
+        //     res.send(item);
+        // });
+        // // PUT
+        // app.put('/tool/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const deliveredQuantity = req.body;
+        //     console.log(deliveredQuantity);
+        //     const filter = { _id: ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updateDoc = {
+        //         $set: {
+        //             quantity: deliveredQuantity.newQuantity
+        //         }
+        //     };
+        //     const result = await toolCollection.updateOne(filter, updateDoc, options);
+        //     res.send(result);
+        // });
+
+        // app.put('/tool/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const setQuantity = req.body;
+        //     const filter = { _id: ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updateDoc = {
+        //         $set: {
+        //             quantity: setQuantity.newQuantity
+        //         }
+        //     };
+        //     const result = await toolCollection.updateOne(filter, updateDoc, options);
+        //     res.send(result);
+        // });
+        // 
     }
     finally {
 
